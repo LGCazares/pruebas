642ed75dd9e4e65d00685752f371e4c3c2691683
LGCazares <luisgerardocazares@gmail.com>
2019-09-21 12:33:37 -0500

    primer commmit

---
+++ resources/views/bienvenido/inicio.blade.php
@@ -0,0 +1,117 @@
+
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<title>BIENVENIDO</title>
+
+	<style type="text/css">
+
+		.bt-01 {
+			color: #000;
+			display: inline-block;
+			font-family: "Open Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
+			padding: 10px 15px;
+			font-weight: 300;
+			font-size: 16px;
+			line-height: 25px;
+			color-interpolation-filters: 
+		}
+
+	</style>	
+</head>
+<body>
+	<div class="m-5">
+		<div class="btn-group mt-3">
+			<img src="images/gobmxlogo.svg" style="height:29px;" alt=" ">
+		</div>	
+		<div style="float:right;">
+			<button class="btn col-2 bt-01">Tramites</button>
+			<button class="btn col-2 bt-01">Gobierno</button>
+			<button class="btn col-2 bt-01">Participa</button>
+			<button class="btn col-2 bt-01">Datos</button>
+			<button class="btn col-2 bt-01"><img src="images/lupa.png" style="height:20px" ></button>
+		</div>
+		<div class="col-12">
+			<img src="images/educa.png" class="col-6" style="height:150px">
+			<img src="images/ipn.png" class="col-1" style="height:150px">
+			holss a tooofsdasd
+		</div>
+
+		<div class="m-5">
+			<table class="table mt-5">
+				<thead class="thead-dark">
+					<tr>
+						<th scope="col">#</th>
+						<th scope="col">First</th>
+						<th scope="col">Last</th>
+						<th scope="col">Handle</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope="row">1</th>
+						<td>Mark</td>
+						<td>Otto</td>
+						<td>@mdo</td>
+					</tr>
+					<tr>
+						<th scope="row">2</th>
+						<td>Jacob</td>
+						<td>Thornton</td>
+						<td>@fat</td>
+					</tr>
+					<tr>
+						<th scope="row">3</th>
+						<td>Larry</td>
+						<td>the Bird</td>
+						<td>@twitter</td>
+					</tr>
+				</tbody>
+			</table>
+
+			<table class="table">
+				<thead class="thead-light">
+					<tr>
+						<th scope="col">#</th>
+						<th scope="col">First</th>
+						<th scope="col">Last</th>
+						<th scope="col">Handle</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope="row">1</th>
+						<td>Mark</td>
+						<td>Otto</td>
+						<td>@mdo</td>
+					</tr>
+					<tr>
+						<th scope="row">2</th>
+						<td>Jacob</td>
+						<td>Thornton</td>
+						<td>@fat</td>
+					</tr>
+					<tr>
+						<th scope="row">3</th>
+						<td>Larry</td>
+						<td>the Bird</td>
+						<td>@twitter</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+
+
+
+
+
+
+
+
+
+
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	</body>
+	</html>
---
+++ resources/views/bienvenido/maqueta01.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MAQUETA</title>
+	<meta charset="utf-8">
+
+	<style type="text/css">
+		.contenedor{
+			width: 1500px;
+			height: 800px;
+			background: #A7D0E1;
+			margin:0.5em auto;
+			padding:0.05em; 
+		}
+		.header{
+			height: 20%;
+			background: #697980;
+			border-radius: 1em; 
+			margin: 1em;
+		}
+		.nav{
+			height: 10%;
+			background:#164B63;
+			border-radius: 1em; 
+			margin: 1em;
+		}
+		.contenido{
+			height: 50%;
+			background:#A7D0E1;
+			margin: 1em;
+		}	
+		.fooder{
+			height: 10%;
+			background:#297090;
+			border-radius: 1em; 
+			margin: 1em;
+		}
+		.section{
+			height: 95%;
+			width: 70%;
+			background: #48ACD6; 
+			border-radius: 1em;
+			float:left;
+			margin: 0.5em;
+		}
+		.aside{
+			height: 95%;
+			width: 27%;
+			background: #48ACD6; 
+			border-radius: 1em;
+			float:right;
+			margin: 0.5em;
+		}
+		.arti{
+			width: 98.5%;
+			height:20%;
+			background: #035679;
+			border-radius: 1em; 
+			margin: 0.5em;
+		}
+	</style>
+</head>
+<body>
+	<div class="contenedor">
+		<div class="header">HEADER</div>
+		<div class="nav">NAV</div>
+		<div class="contenido">
+			<div class="section">SECTION
+				<article class="arti">
+					<h1>Hola como estan todo</h1>
+				</article>
+				<article class="arti">
+					<p>aqui no se que poner</p>
+				</article>
+				<article class="arti">
+					aqui menos 
+				</article>
+				<article class="arti">aqui tampoco
+				</article> 
+			</div>
+			<div class="aside">ASIDE</div>
+		</div>
+		<div class="fooder">FOODER</div>
+	</div>
+
+
+</body>
+</html>
